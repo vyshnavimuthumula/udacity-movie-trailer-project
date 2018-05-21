a9a1a59105fa3863df0a4facb35209101cb174b0
vyshnavimuthumula <38848887+vyshnavimuthumula@users.noreply.github.com>
2018-05-21 22:50:54 +0530

    Update media.py

--- media.py
+++ media.py
@@ -6,7 +6,7 @@ class Movie():
 
     def __init__(self, title, year, poster_url, trailer_url):
         """ Inits a Movie object 
-        Args:
+        Arguments:
         title = a string of the movie's title
         year = an integer for the year of the movie's release
         poster_url = a string containing a URL to a poster image
